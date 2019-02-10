cb05010db6f9dec28310317927d9f1cede324459
c h <me@chicks06.co.uk>
2019-02-10 14:54:01 +0000

    Refactor colour/tile generators and add in rest of kitchen

--- wall.js
+++ wall.js
@@ -1,7 +1,6 @@
 var house;
 var storeyHeight;
 var sideWallLength;
-var innerWallOffset;
 
 function drawHouse(){
 	//draws a 2 story house
@@ -9,48 +8,70 @@ function drawHouse(){
 	//room height = roof height
 	
 	house = document.getElementById("house");
+	setHouseSize();
 	
 	//draw external wall
 	drawBrickWall();
 	//add windows and door;
-	
-	//add floor
-	
-	//draw inner wall
-	drawInnerWall();
-	
-	//add windows and door
+	drawWindows();
 
-	//add floor
+	//draw left room
+	drawKitchen();
+	
+	//add right room
+	drawLounge();
+
+	//draw bedroom
+	drawBedroom();
 	
 	//draw roof inc ceiling
-	
-	//draw first floor
+
 }
 
-function placeBrick(x, y, length, height){
-	var newBrick = createBrick(height, length);
+function setHouseSize(){
+	sideWallLength = 3000;
+	storeyHeight =  2990;
+}
+
+function drawRectangle(x, y, length, height, colourType){
+	var newBrick = document.createElementNS(svgNS, "rect");
+	newBrick.setAttributeNS(null, "height", height);
+	newBrick.setAttributeNS(null, "width", length);
 	newBrick.setAttributeNS(null, "x", x); 
 	newBrick.setAttributeNS(null, "y", y); 
-	newBrick.setAttributeNS(null, "class", "brick"); 
+	
+	newBrick.setAttributeNS(null, "fill", selectColour(colourType) 	);
+		
 	house.appendChild(newBrick);
 }
 
+function selectColour(type){
+	switch (type){
+		case "RedBrick":
+			return "hsl("+(0+Math.round(Math.random()*20))+", "+(40-10+Math.round(Math.random()*20))+"%, "+(40-10+Math.round(Math.random()*5))+"%)";
+		case "KitchenTile":
+			return "hsl("+(70+Math.round(Math.random()*10))+", "+(40-10+Math.round(Math.random()*20))+"%, "+(30-10+Math.round(Math.random()*5))+"%)";
+		default:
+			return type;
+	}
+}
+
 function drawBrickWall() {
-	var wallLengthSide = 6;
-	var wallLengthBack = 12;
+	/*define brick size*/
 	var brickLength = 500;
+	var brickHeight = 130;
 	var halfLength = brickLength/2;
 	var cornerLength = halfLength*3;
+
+	/*define length and height of wall in bricks*/
+	var wallLengthSide = sideWallLength/brickLength;
+	var wallLengthBack = wallLengthSide * 2;
 	
-	var wallHeight =  46;
-	var brickHeight = 130;
+	var wallHeight =  storeyHeight/brickHeight*2;
 
 	/*create the wall*/
 	var yPosition = 0;
 	var startWithHalfBrick = false;
-	sideWallLength = wallLengthSide * brickLength;
-	storeyHeight = wallHeight * brickHeight / 2;
 	
 	for (var y=0; y < wallHeight; y++) {
 		//We are drawing a side, followed by the back, followed by another side
@@ -59,38 +80,38 @@ function drawBrickWall() {
 		var xPosition = sideWallLength * 2;
 
 		if(startWithHalfBrick){
-			placeBrick( xPosition, yPosition, halfLength, brickHeight);
+			drawRectangle( xPosition, yPosition, halfLength, brickHeight, "RedBrick");
 			xPosition += halfLength;
 		}
 		
 		//first side
 		for (var x = 0; x < wallLengthSide - 1; x++){
-			placeBrick(xPosition, yPosition, brickLength, brickHeight);
+			drawRectangle(xPosition, yPosition, brickLength, brickHeight, "RedBrick");
 			xPosition += brickLength;
 		}
 
 		//corner
-		placeBrick(xPosition, yPosition, cornerLength, brickHeight);
+		drawRectangle(xPosition, yPosition, cornerLength, brickHeight, "RedBrick");
 		xPosition += cornerLength;
 		
 		//back
 		for (var x1 = 0; x1 < wallLengthBack - (startWithHalfBrick ? 2 : 1); x1++){
-			placeBrick(xPosition, yPosition, brickLength, brickHeight);
+			drawRectangle(xPosition, yPosition, brickLength, brickHeight, "RedBrick");
 			xPosition += brickLength;
 		}
 
 		//corner
-		placeBrick(xPosition, yPosition, cornerLength, brickHeight);
+		drawRectangle(xPosition, yPosition, cornerLength, brickHeight, "RedBrick");
 		xPosition += cornerLength;
 		
 		//second side
 		for (var x2 = 0; x2 < wallLengthSide -1 ; x2++){
-			placeBrick(xPosition, yPosition, brickLength, brickHeight);
+			drawRectangle(xPosition, yPosition, brickLength, brickHeight, "RedBrick");
 			xPosition += brickLength;
 		}			
 		
 		if(startWithHalfBrick){
-			placeBrick( xPosition, yPosition, halfLength, brickHeight);
+			drawRectangle( xPosition, yPosition, halfLength, brickHeight, "RedBrick");
 		}
 		
 		startWithHalfBrick = !startWithHalfBrick;
@@ -114,35 +135,46 @@ function drawBrickWall() {
 	house.appendChild(roofMask);
 }
 
-
-function drawInnerWall(){
+function drawKitchen(){
 
 	// left room
-	var innerWallLeft = document.createElementNS(svgNS, "path");
-	
-	var path = "M0 " + storeyHeight +
-				" L" + sideWallLength * 2 + " " + storeyHeight +
-				" L" + sideWallLength * 2 + " " + storeyHeight * 2 +
-				" L0" + " " + storeyHeight * 2 +
-				" Z";
+	drawRectangle(0, storeyHeight, sideWallLength * 2, storeyHeight, "#dfefdf");
 				
-	innerWallLeft.setAttributeNS(null, "d", path);
-	innerWallLeft.setAttributeNS(null, "fill", "#dfefdf");
-	house.appendChild(innerWallLeft);
+	//kitchen furniture
+	drawRectangle(sideWallLength * 0.07, storeyHeight * 1.7, sideWallLength * 0.4, storeyHeight * 0.3, "#fefefe");
+	drawRectangle(sideWallLength * 0.55, storeyHeight * 1.25, sideWallLength * 0.4, storeyHeight * 0.75, "url(#fridge)");
 	
-	//right room
-	var innerWall = document.createElementNS(svgNS, "path");
+	//draw floor
+	var numberOfTiles = 7;
+	var tileSize = sideWallLength/numberOfTiles;
 	
-	var path = "M" + sideWallLength * 6 + " " + storeyHeight +
-			" L" + sideWallLength * 8 + " " + storeyHeight +
-			" L" + sideWallLength * 8 + " " + storeyHeight * 2 +
-			" L" + sideWallLength * 6 + " " + storeyHeight * 2 +
-			" Z";
-				
-	innerWall.setAttributeNS(null, "d", path);
-	innerWall.setAttributeNS(null, "fill", "#FFE4E1");
-	house.appendChild(innerWall);
+	var yPosition = storeyHeight * 2;
+	for (var y=0; y < numberOfTiles; y++) {
+		var xPosition = 0;
+	
+		//first side
+		for (var x = 0; x < numberOfTiles; x++){
+			drawRectangle(xPosition, yPosition, tileSize, tileSize,"KitchenTile");
+			xPosition += tileSize;
+		}
 
+		yPosition += tileSize;
+	}
+}
+
+function drawLounge(){
+	//right room
+	drawRectangle(sideWallLength * 6, storeyHeight, sideWallLength * 2, storeyHeight, "#FFE4E1");
+	
+	//lounge furniture
+	
+	
+	//draw floor
+
+}
+
+function drawBedroom(){
+	
 	//roof wall
 	var innerWallRoof = document.createElementNS(svgNS, "path");
 	
@@ -154,4 +186,8 @@ function drawInnerWall(){
 	innerWallRoof.setAttributeNS(null, "d", path);
 	innerWallRoof.setAttributeNS(null, "fill", "#FFF8DC");
 	house.appendChild(innerWallRoof);
+	
+	//draw floor
 }
+
+function drawWindows(){}
