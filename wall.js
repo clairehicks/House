010f05ec31567fb0af73090a58392e07a37326a6
c h <me@chicks06.co.uk>
2019-02-10 00:56:13 +0000

    added basic inner walls

--- wall.js
+++ wall.js
@@ -1,8 +1,31 @@
 var house;
+var storeyHeight;
+var sideWallLength;
+var innerWallOffset;
 
 function drawHouse(){
+	//draws a 2 story house
+	//width = 2 * depth
+	//room height = roof height
+	
 	house = document.getElementById("house");
-	drawWall(house);
+	
+	//draw external wall
+	drawBrickWall();
+	//add windows and door;
+	
+	//add floor
+	
+	//draw inner wall
+	drawInnerWall();
+	
+	//add windows and door
+
+	//add floor
+	
+	//draw roof inc ceiling
+	
+	//draw first floor
 }
 
 function placeBrick(x, y, length, height){
@@ -13,7 +36,7 @@ function placeBrick(x, y, length, height){
 	house.appendChild(newBrick);
 }
 
-function drawWall(house) {
+function drawBrickWall() {
 	var wallLengthSide = 6;
 	var wallLengthBack = 12;
 	var brickLength = 500;
@@ -26,16 +49,18 @@ function drawWall(house) {
 	/*create the wall*/
 	var yPosition = 0;
 	var startWithHalfBrick = false;
-
+	sideWallLength = wallLengthSide * brickLength;
+	storeyHeight = wallHeight * brickHeight / 2;
+	
 	for (var y=0; y < wallHeight; y++) {
 		//We are drawing a side, followed by the back, followed by another side
 		//Note that when going round corners, bricks are 1.5*brick length
 
-		var xPosition = 0;
+		var xPosition = sideWallLength * 2;
 
 		if(startWithHalfBrick){
-			placeBrick( 0, yPosition, halfLength, brickHeight);
-			xPosition = halfLength;
+			placeBrick( xPosition, yPosition, halfLength, brickHeight);
+			xPosition += halfLength;
 		}
 		
 		//first side
@@ -74,19 +99,59 @@ function drawWall(house) {
 	
 	//add roof cutout
 	var roofMask = document.createElementNS(svgNS, "path");
-	var quarterHouseLength = wallLengthSide * brickLength;
-	var roofHeight = wallHeight * brickHeight / 2;
 	
-	var path = "M0 0" +
-				" L" + quarterHouseLength * 4 + " 0"+
-				" L" + quarterHouseLength * 4 + " " + roofHeight + 
-				" L" + quarterHouseLength * 3 + " " + roofHeight + 
-				" L" + quarterHouseLength * 2 + " 0" +
-				" L" + quarterHouseLength + " " + roofHeight +
-				" L0 " + roofHeight +
+	var path = "M" + sideWallLength + " 0" +
+				" L" + sideWallLength * 6 + " 0"+
+				" L" + sideWallLength * 6 + " " + storeyHeight + 
+				" L" + sideWallLength * 5 + " " + storeyHeight + 
+				" L" + sideWallLength * 4 + " 0" +
+				" L" + sideWallLength * 3 + " " + storeyHeight +
+				" L0 " + storeyHeight +
 				" Z";
 				
 	roofMask.setAttributeNS(null, "d", path);
 	roofMask.setAttributeNS(null, "fill", "#fff");
 	house.appendChild(roofMask);
+}
+
+
+function drawInnerWall(){
+
+	// left room
+	var innerWallLeft = document.createElementNS(svgNS, "path");
+	
+	var path = "M0 " + storeyHeight +
+				" L" + sideWallLength * 2 + " " + storeyHeight +
+				" L" + sideWallLength * 2 + " " + storeyHeight * 2 +
+				" L0" + " " + storeyHeight * 2 +
+				" Z";
+				
+	innerWallLeft.setAttributeNS(null, "d", path);
+	innerWallLeft.setAttributeNS(null, "fill", "#dfefdf");
+	house.appendChild(innerWallLeft);
+	
+	//right room
+	var innerWall = document.createElementNS(svgNS, "path");
+	
+	var path = "M" + sideWallLength * 6 + " " + storeyHeight +
+			" L" + sideWallLength * 8 + " " + storeyHeight +
+			" L" + sideWallLength * 8 + " " + storeyHeight * 2 +
+			" L" + sideWallLength * 6 + " " + storeyHeight * 2 +
+			" Z";
+				
+	innerWall.setAttributeNS(null, "d", path);
+	innerWall.setAttributeNS(null, "fill", "#FFE4E1");
+	house.appendChild(innerWall);
+
+	//roof wall
+	var innerWallRoof = document.createElementNS(svgNS, "path");
+	
+	var path = "M" + sideWallLength * 7 + " " + storeyHeight +
+				" L" + sideWallLength * 8 + " 0" +
+				" L" + sideWallLength * 9 + " " + storeyHeight + 
+				" Z";
+				
+	innerWallRoof.setAttributeNS(null, "d", path);
+	innerWallRoof.setAttributeNS(null, "fill", "#FFF8DC");
+	house.appendChild(innerWallRoof);
 }
